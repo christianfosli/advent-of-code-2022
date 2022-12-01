cec33c234aee8d372103da6bc6c2454f9f08bcb8
Christian Fosli <christian.fosli@capgemini.com>
2022-12-01 16:24:37 +0100

    refactor: Slightly simplify aoc01

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -2,18 +2,20 @@ use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = fs::read_to_string("input.txt")?;
-    let calories_by_elf = input.split("\n\n").map(|elf| {
-        elf.lines()
-            .filter_map(|cal| cal.parse::<u64>().ok())
-            .sum::<u64>()
-    });
+    let mut calories_by_elf = input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .filter_map(|cal| cal.parse::<u64>().ok())
+                .sum::<u64>()
+        })
+        .collect::<Vec<_>>();
 
     println!(
         "Part 1 - elf with most calories: {:?}",
-        calories_by_elf.clone().max()
+        calories_by_elf.iter().max()
     );
 
-    let mut calories_by_elf = calories_by_elf.collect::<Vec<_>>();
     calories_by_elf.sort_unstable();
     println!(
         "Part 2 answer - sum of top 3 elve's calories: {:?}",
