86503307753a60378c22952eb6dcba9f30cd057f
Christian Fosli <christian.fosli@capgemini.com>
2022-12-04 19:14:30 +0100

    Day 4 - part 2

--- aoc04/src/main.rs
+++ aoc04/src/main.rs
@@ -1,27 +1,44 @@
-use std::{error::Error, fs};
+use std::{collections::HashSet, error::Error, fs, ops::RangeInclusive};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("input.txt")?;
-    println!("{}", part_1_count_full_overlaps(&input));
+    println!("1 - full overlaps: {}", part_1_count_full_overlaps(&input));
+    println!("2 - any overlaps: {}", part_2_count_any_overlaps(&input));
     Ok(())
 }
 
-fn part_1_count_full_overlaps(assignment_pairs: &str) -> usize {
+fn to_range_pair(pair: &str) -> (RangeInclusive<usize>, RangeInclusive<usize>) {
     let into_range = |r: &str| {
         let (from, to) = r.split_once('-').unwrap();
         from.parse::<usize>().unwrap()..=to.parse::<usize>().unwrap()
     };
 
+    let (p1, p2) = pair.split_once(',').unwrap();
+    (into_range(p1), into_range(p2))
+}
+
+fn part_1_count_full_overlaps(assignment_pairs: &str) -> usize {
     assignment_pairs
         .lines()
-        .map(|pair| {
-            let (p1, p2) = pair.split_once(',').unwrap();
-            (into_range(p1), into_range(p2))
-        })
+        .map(to_range_pair)
         .filter(|(p1, p2)| {
             (p1.contains(&p2.clone().next().unwrap()) && p1.contains(&p2.clone().last().unwrap()))
                 || (p2.contains(&p1.clone().next().unwrap())
                     && p2.contains(&p1.clone().last().unwrap()))
+        })
+        .count()
+}
+
+fn part_2_count_any_overlaps(assignment_pairs: &str) -> usize {
+    assignment_pairs
+        .lines()
+        .map(to_range_pair)
+        .filter(|(p1, p2)| {
+            p1.clone()
+                .collect::<HashSet<_>>()
+                .intersection(&p2.clone().collect::<HashSet<_>>())
+                .next()
+                .is_some()
         })
         .count()
 }
@@ -40,5 +57,10 @@ mod tests {
     #[test]
     fn it_works_with_example_1() {
         assert_eq!(2, part_1_count_full_overlaps(TEST_ASSIGNMENT_PAIRS));
+    }
+
+    #[test]
+    fn it_works_with_example_2() {
+        assert_eq!(4, part_2_count_any_overlaps(TEST_ASSIGNMENT_PAIRS));
     }
 }
