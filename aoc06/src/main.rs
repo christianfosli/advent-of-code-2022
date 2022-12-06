9b7cfad3efdb7471708e9f934da70081eb4704e8
Christian Fosli <christian.fosli@capgemini.com>
2022-12-06 21:57:50 +0100

    Day 06 part 2

--- aoc06/src/main.rs
+++ aoc06/src/main.rs
@@ -6,30 +6,38 @@ use itertools::Itertools;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("input.txt")?;
-    println!("Part 1: {:?}", part_1_find_first_start_of_packet(&input));
+    println!("Part 1: {:?}", part_1_find_start_of_packet(&input));
+    println!("Part 2: {:?}", part_2_find_start_of_msg(&input));
     Ok(())
 }
 
-fn part_1_find_first_start_of_packet(datastream: &str) -> (Option<usize>, Vec<char>) {
-    datastream
-        .chars()
+fn part_1_find_start_of_packet(datastream: &str) -> (Option<usize>, Vec<char>) {
+    find_first_unique::<4>(datastream)
+}
+
+fn part_2_find_start_of_msg(datastream: &str) -> (Option<usize>, Vec<char>) {
+    find_first_unique::<14>(datastream)
+}
+
+fn find_first_unique<const N: usize>(s: &str) -> (Option<usize>, Vec<char>) {
+    s.chars()
         .enumerate()
         .fold_while((None, Vec::new()), |(_, mut marker), (i, el)| {
             if marker.iter().all(|&x| x != el) {
                 marker.push(el);
-                if marker.len() == 4 {
+                if marker.len() == N {
                     Done((Some(i + 1), marker))
                 } else {
-                    Continue((Some(i + 1), marker))
+                    Continue((None, marker))
                 }
             } else {
                 let (_, rest) = marker.split_at(marker.iter().position(|&x| x == el).unwrap() + 1);
                 let marker = rest
-                    .into_iter()
-                    .cloned()
+                    .iter()
+                    .copied()
                     .chain(iter::once(el))
                     .collect::<Vec<_>>();
-                Continue((Some(i + 1), marker))
+                Continue((None, marker))
             }
         })
         .into_inner()
@@ -43,9 +51,11 @@ mod tests {
 
     #[test]
     fn it_works_with_example_1() {
-        assert_eq!(
-            Some(7),
-            part_1_find_first_start_of_packet(TEST_DATASTREAM).0
-        );
+        assert_eq!(Some(7), part_1_find_start_of_packet(TEST_DATASTREAM).0);
+    }
+
+    #[test]
+    fn it_works_with_example_2() {
+        assert_eq!(Some(19), part_2_find_start_of_msg(TEST_DATASTREAM).0);
     }
 }
