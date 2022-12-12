3a9627d18f852bef10715d204a1d72ec270a3ba3
Christian Fosli <christian.fosli@capgemini.com>
2022-12-12 22:37:11 +0100

    Minor adjustments

--- aoc11/src/main.rs
+++ aoc11/src/main.rs
@@ -27,7 +27,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("input.txt")?;
     let monkeys = parse_monkeys(&input)?;
     println!("Part 1: {}", part_1_level_of_monkey_business(&monkeys));
-    println!("Part 2: {}", part_2_level_of_monkey_business(&monkeys));
+    println!("Part 2: TODO");
     Ok(())
 }
 
@@ -39,10 +39,7 @@ fn parse_monkeys(s: &str) -> Result<Vec<Monkey>, Box<dyn Error>> {
 
     Ok(s.split("\n\n")
         .map(|x| {
-            let mut monkey_data = x
-                .lines()
-                .skip(1)
-                .filter_map(|line| line.split(": ").skip(1).next());
+            let mut monkey_data = x.lines().skip(1).filter_map(|line| line.split(": ").nth(1));
             let items = monkey_data
                 .next()
                 .unwrap()
@@ -149,7 +146,7 @@ fn part_1_level_of_monkey_business(monkeys: &[Monkey]) -> usize {
     most_active.1 * second_most_active.1
 }
 
-fn part_2_level_of_monkey_business(monkeys: &[Monkey]) -> usize {
+fn _part_2_level_of_monkey_business(monkeys: &[Monkey]) -> usize {
     // TODO: Overflows during multiplication
     let mut monkeys = monkeys.to_vec();
     let mut inspections = HashMap::new();
